dba73e8484f7baae16bb8783da0f8f2c1989c276
Mario Campos <mario-campos@github.com>
2021-09-28 21:21:17 -0500

    Write the hijack script at run-time
    This saves the need to store another environment variable, EMULATE_VAGRANT_PATH. It leads to a slightly cleaner workflow debug.

--- hijack.sh
+++
@@ -1,9 +0,0 @@
-# Store the parent shell's command line into the $BASH_ARGS array. 
-read -r -a BASH_ARGS <<< "$(ps -p $$ -o args=)"
-
-# Because macOS has bash < 4.2, we can't use the convenient '${array[-1]}'
-# syntax. But, '${array[@]: -1:1}' is supported.
-LAST_ARG="${BASH_ARGS[@]: -1:1}"
-
-cd "$EMULATE_VAGRANT_PATH"
-exec sudo vagrant ssh -c "$(cat "$LAST_ARG")"
